5d263b52831af0f1239c4e251c7bc7f17a02c6af
EC2 Default User <ec2-user@ip-172-31-3-0.ap-northeast-1.compute.internal>
2024-05-21 06:35:10 +0000

    [Add] バリテーションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,25 +6,26 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    @user = current_user
     if @book.save
     flash[:notice] = "You have created book successfully."
 
-    redirect_to books_path
+    redirect_to users_path(@book.id)
     else
     @books = Book.all
-    render :show
+    render :index
     end
   end
 
   def index
   @books = Book.all
-
+  @book = Book.new
   @user = current_user
   end
 
   def show
   @book = Book.find(params[:id])
-  @user = User.find(params[:id])
+  @user = current_user
   end
 
   def edit
@@ -32,9 +33,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
     redirect_to book_path(book.id)
+    else
+    render :edit
+    end
   end
 
   def destroy
